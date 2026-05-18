69cd829935adb19f7adad8b94351baf75a363835
hoanganh04-11 <ht.anh00411@gmail.com>
2026-05-18 15:39:05 +0700

    OOP_TEL: OOP004 Point Class

--- OOP004.java
+++ OOP004.java
@@ -11,6 +11,11 @@ class Point {
     private int x;
     private int y;
     
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
     public int getX(){
         return this.x;
     }
@@ -28,8 +33,8 @@ class Point {
     }
     
     public void move(int dx, int dy){
-        x += dx;
-        y += dy;
+        this.x = x + dx;
+        this.y = y + dy;
     }
     
     public double distanceTo(Point p){
@@ -37,7 +42,7 @@ class Point {
         int y1 = this.y;
         int x2 = p.x;
         int y2 = p.y;
-        return Math.sqrt((x1-x2) * (x1-x2) + (y1-y2)*(y1-y2));
+        return Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));
     }
 }
 
@@ -47,20 +52,14 @@ public class OOP004 {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while(t-- > 0){
-            Point A = new Point();
-            int xA = sc.nextInt();
-            A.setX(xA);
-            int yA = sc.nextInt();
-            A.setY(yA);
-            A.move(1, 1);
-            
-            Point B = new Point();
-            int xB = sc.nextInt();
-            B.setX(xB);
-            int yB = sc.nextInt();
-            B.setY(yB);
-            
-            System.out.printf("%.2f\n", A.distanceTo(B));
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            Point a = new Point(x1, y1);
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+            Point b = new Point(x2, y2);
+            a.move(1, 1);
+            System.out.printf("%.2f\n", a.distanceTo(b));
         }
     }
 }
